4a8cd5749cea038cead18446baf0e4ad1ed92055
Alexander <alexanderbenitezrivpla97@gmail.com>
2026-06-28 20:19:34 -0300

    feat: agrega rutas para mascotas en web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PetController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () {return view('welcome');});
+Route::get('/mascotas', [PetController::class, 'index'])->name('pets.index');
+Route::get('/mascotas/crear', [PetController::class, 'create'])->name('pets.create');
+Route::post('/mascotas', [PetController::class, 'store'])->name('pets.store');
